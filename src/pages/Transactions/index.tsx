a74490e1be830f224d4b4aab2ebeabfbecf23a9f
João Sanson <joaosanson01@gmail.com>
2023-09-12 20:32:52 -0300

    feat: creating transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,6 @@
 import { Header } from '../../components/Header'
 import { Summary } from "../../components/Summary"
-import { TransactionsContainer } from "./styles"
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
 
 export function Transactions() {
   return (
@@ -9,30 +9,36 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
-        <table>
+        <TransactionsTable>
           <tbody>
             <tr>
               <td width="50%">Desenvolvimento de site</td>
-              <td>R$ 12.000,00</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
               <td>Venda</td>
               <td>13/04/2022</td>
             </tr>
 
             <tr>
               <td >Hamburger</td>
-              <td>- R$ 59,00</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
               <td>Alimentação</td>
               <td>10/04/2022</td>
             </tr>
 
             <tr>
               <td>Aluguel do apartamento</td>
-              <td>- R$ 1.200,00</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 1.200,00</PriceHighlight>
+              </td>
               <td>Casa</td>
               <td>27/03/2022</td>
             </tr>
           </tbody>
-        </table>
+        </TransactionsTable>
       </TransactionsContainer>
     </>
   )
