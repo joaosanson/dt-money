7c738bf82f66a2f7b6effada0015aeba0a19686f
João Sanson <joaosanson01@gmail.com>
2023-09-12 11:20:44 -0300

    feat: creating transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../components/Header'
 import { Summary } from "../../components/Summary"
+import { TransactionsContainer } from "./styles"
 
 export function Transactions() {
   return (
     <>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <table>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td >Hamburger</td>
+              <td>- R$ 59,00</td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+
+            <tr>
+              <td>Aluguel do apartamento</td>
+              <td>- R$ 1.200,00</td>
+              <td>Casa</td>
+              <td>27/03/2022</td>
+            </tr>
+          </tbody>
+        </table>
+      </TransactionsContainer>
     </>
   )
 }
