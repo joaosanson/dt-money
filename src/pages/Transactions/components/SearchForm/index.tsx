a5bc0cd6df41eff4d2891e32fd389f1694cbf1af
João Sanson <joaosanson01@gmail.com>
2023-09-13 10:00:12 -0300

    feat: starting search form

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,10 @@
+import { SearchFormContainer } from "./styles";
+
+export function SearchForm() {
+  return(
+    <SearchFormContainer>
+      <input type="text" />
+      <button>Enviar</button>
+    </SearchFormContainer>
+  )
+}
---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+  }
+`
