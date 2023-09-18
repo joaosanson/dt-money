f72645ea416c97ada9c341884bf926888eb98a06
João Sanson <joaosanson01@gmail.com>
2023-09-18 11:04:21 -0300

    perf: creating features to imporve performance

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -6,12 +6,14 @@ import {
   TransactionsTable,
 } from './styles'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
-import { useContext } from 'react'
 import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { SearchForm } from './components/SearchForm'
+import { useContextSelector } from 'use-context-selector'
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   return (
     <>
